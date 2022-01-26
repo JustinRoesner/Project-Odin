d597302cba35e62632f48daad95cbe9c4334200b
Justin <justinroesner@gmail.com>
2022-01-26 16:44:35 -0600

    working sound on click and on keydown

--- webboot drumkit/index.js
+++ webboot drumkit/index.js
@@ -6,7 +6,65 @@ function handleClick(){
 }
 */
 
-
+// listen for button click
 for (var i = 0; i<7; i++){
-    document.querySelectorAll(".drum")[i].addEventListener("click", function (){ alert("hi")});
+    // document.querySelectorAll(".drum")[i].addEventListener("click", function (){ alert("hi")});
+    document.querySelectorAll(".drum")[i].addEventListener("click", function (){  
+        // console.log(this.innerHTML);
+        // this.style.color = "white";
+        var buttonInnerHTML = this.innerHTML;
+        makeSound(buttonInnerHTML);
+    });
 }
+
+
+// listen for key press
+document.addEventListener("keydown", function(event) {
+    // alert("Hey was pressed!");
+    // console.log(event.key);
+    makeSound(event.key);
+});
+
+function makeSound(key){
+        switch (key) {
+            case "w":
+                var tom1 = new Audio('sounds/tom-1.mp3');
+                tom1.volume = 0.1;
+                tom1.play()
+                break;
+            case "a":
+                var tom2 = new Audio('sounds/tom-2.mp3');
+                tom2.volume = 0.1;
+                tom2.play()
+                break;
+            case "s":
+                var tom3 = new Audio('sounds/tom-3.mp3');
+                tom3.volume = 0.1;
+                tom3.play()
+                break;
+            case "d":
+                var tom4 = new Audio('sounds/tom-4.mp3');
+                tom4.volume = 0.1;
+                tom4.play()
+                break;
+            case "j":
+                var crash = new Audio('sounds/crash.mp3');
+                crash.volume = 0.1;
+                crash.play()
+                break;
+            case "k":
+                var bass = new Audio('sounds/kick-bass.mp3');
+                bass.volume = 0.1;
+                bass.play()
+                break;
+            case "l":
+                var audio = new Audio('sounds/crash.mp3');
+                audio.volume = 0.1;
+                audio.play()
+                break;
+            default:
+                console.log(buttonInnerHTML);
+                break;
+        }
+}
+
