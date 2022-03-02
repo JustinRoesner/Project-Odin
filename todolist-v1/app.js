258a433c3e2e2cabd26a06ca2cc0419fe84d2ce5
geothcloak <justinroesner@gmail.com>
2022-03-01 20:05:12 -0600

    boilerplate

---
+++ todolist-v1/app.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+app.get("/", function(req, res){
+    var today = new Date();
+    if (today.getDay() === 6 || today.getDay() === 0){
+        res.write("<h1>Yay it's the weekend!</h1>");
+    } else {
+        // res.write("<p>It is not the weekend.</p>");
+        // res.write("<h1>Boo! I have to work!</h1>");
+        // res.send();
+
+        res.sendFile(__dirname + "/index.html");
+    }
+});
+
+app.listen(3000, function(){
+    console.log("Server started on port 3000");
+});
