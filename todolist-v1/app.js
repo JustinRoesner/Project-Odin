5df952bfc8e06c042b4d290f9b2c92143139b056
geothcloak <justinroesner@gmail.com>
2022-03-13 15:22:35 -0500

    add templating using ejs

--- todolist-v1/app.js
+++ todolist-v1/app.js
@@ -3,17 +3,57 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
+app.set("view engine", "ejs");
+
 app.get("/", function(req, res){
     var today = new Date();
+    var day = "";
+
+    var currentDay = today.getDay();
+
+    switch (currentDay) {
+        case 0:
+            day = "Sunday";
+            break;
+        case 1:
+            day = "Monday";
+            break;
+        case 2:
+            day = "Tuesday";
+            break;
+        case 3:
+            day = "Wednesday";
+            break;
+        case 4:
+            day = "Thursday";
+            break;
+        case 5:
+            day = "Friday";
+            break;
+        case 6:
+            day = "Saturday";
+            break;
+        default:
+            console.log("Error: Day out of range. day = " + currentDay);
+            break;
+    }
+
+    /*
     if (today.getDay() === 6 || today.getDay() === 0){
-        res.write("<h1>Yay it's the weekend!</h1>");
+        day = "Weekend";
+        //res.write("<h1>Yay it's the weekend!</h1>");
+        // res.sendFile(__dirname + "/weekend.html");
     } else {
+        day = "Weekday";
+        // res.sendFile(__dirname + "/weekday.html");
         // res.write("<p>It is not the weekend.</p>");
         // res.write("<h1>Boo! I have to work!</h1>");
         // res.send();
 
-        res.sendFile(__dirname + "/index.html");
+        //res.sendFile(__dirname + "/index.html");
     }
+    */
+        res.render("list", {kindOfDay: day});
 });
 
 app.listen(3000, function(){
