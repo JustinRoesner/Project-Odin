f869ca0d4c42a586ae185d6e6fd637a4736e3b88
Justin <justinroesner@gmail.com>
2022-06-07 23:23:34 -0500

    doc select statements

--- JSexpert/complete-javascript-course-master/06-Modal/starter/script.js
+++ JSexpert/complete-javascript-course-master/06-Modal/starter/script.js
@@ -1,1 +1,11 @@
 'use strict';
+
+// start by selecting everything we need and save them into variables
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnsOpenModal = document.querySelectorAll('.show-modal');
+console.log(btnsOpenModal);
+
+for (let i = 0; i < btnsOpenModal.length; i++)
+  console.log(btnsOpenModal[i].textContent);
